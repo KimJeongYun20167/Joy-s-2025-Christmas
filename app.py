4f9188e0bc2f7cf7fb8b7f0f5ae533d5fc32f171
KimJeongYun20167 <keaton20167@gmail.com>
2025-12-26 00:15:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="🎄 Christmas Carol", page_icon="🎄")
 st.title("𝙲𝚑𝚛𝚒𝚜𝚝𝚖𝚊𝚜 𝚊𝚝 𝙷𝚘𝚐𝚠𝚊𝚛𝚝𝚜")
 st.write("숙제하기 싫어서 만든 뻘짓거리♡꙼̈")
 
-audio_path = Path(__file__).parent / "assets" / "carol.mp3"
+audio_path = Path(__file__).parent / "asset" / "carol.mp3"
 
 if audio_path.exists():
     st.audio(audio_path.read_bytes(), format="audio/mp3")
