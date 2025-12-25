5cc148e1b76c3a0f7f56df09f83ca4d5420a0530
KimJeongYun20167 <keaton20167@gmail.com>
2025-12-26 00:10:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 st.set_page_config(page_title="🎄 Christmas Carol", page_icon="🎄")
 
-st.title("𝙲𝚑𝚛𝚒𝚜𝚝𝚖𝚊𝚜 𝚊𝚝 𝙷𝚘𝚐𝚠𝚊𝚛𝚝𝚜 ᙏ̤̮ ")
+st.title("𝙲𝚑𝚛𝚒𝚜𝚝𝚖𝚊𝚜 𝚊𝚝 𝙷𝚘𝚐𝚠𝚊𝚛𝚝𝚜")
 st.write("숙제하기 싫어서 만든 뻘짓거리♡꙼̈")
 
 audio_path = Path(__file__).parent / "assets" / "carol.mp3"
